a3e84246294d9d14ea97a30be35fc59339f2ba97
IgorTeplov <igorteplov2001@gmail.com>
2023-01-27 13:43:00 +0200

    fix /

--- linktrack/urls.py
+++ linktrack/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from linktrack.views import TrackView, TrackWebhook, UrlBuilder
 
 urlpatterns = [
-    path('/', UrlBuilder.as_view()),
+    path('', UrlBuilder.as_view()),
     path('webhook/', TrackWebhook.as_view()),
-    path('<alias>', TrackView.as_view()),
+    path('<alias>/', TrackView.as_view()),
 ]
